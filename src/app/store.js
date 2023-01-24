6b57ecc1e68f546e58fb8fa069b002137db668c7
DESKTOP-3A5KK61\nguye <nguyenhaitan001@gmail.com>
2023-01-24 14:21:56 +0700

    Learnt

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,18 @@
-// import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import postReducer from "../features/post/postSlice";
+import userReducer from "../features/user/userSlice";
+import commentReducer from "../features/comment/commentSlice";
+import friendReducer from "../features/friend/friendSlice";
 
-// export const store = configureStore({
-//     reducer: ""
+const rootReducer = combineReducers({
+  post: postReducer,
+  user: userReducer,
+  comment: commentReducer,
+  friend: friendReducer,
+});
 
-// })
+export const store = configureStore({
+  reducer: rootReducer,
+});
 
-// export default store
-
+export default store;
